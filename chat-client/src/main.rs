3ea9d01bde2f94faa983264f330189999270c1b0
abrasumente <345396594@qq.com>
2022-04-14 23:09:55 +0800

    (chat-client) hello tui-rs

--- chat-client/src/main.rs
+++ chat-client/src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    event::{DisableMouseCapture, EnableMouseCapture},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::{io, thread, time::Duration};
+use tui::{
+    backend::{Backend, CrosstermBackend},
+    widgets::{Block, Borders},
+    Frame, Terminal,
+};
+
+fn draw<B: Backend>(f: &mut Frame<B>) {
+    let size = f.size();
+    let block = Block::default().title("Block").borders(Borders::ALL);
+    f.render_widget(block, size);
+}
+
+fn main() -> Result<(), io::Error> {
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.draw(draw)?;
+
+    thread::sleep(Duration::from_secs(5));
+
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+    //println!("Hello, world!");
+
+    Ok(())
 }
